9a876c898882833500ce22da1acd6382052dc02b
Mikhail Matveichuk <mihkailmatveichuk@gmail.com>
2023-11-23 21:51:51 +0300

    feat: add profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,15 @@
+import { useSession} from 'next-auth/react'
 import React from 'react'
+import { AuthConfig } from '../../../config/auth'
+import { getServerSession } from 'next-auth'
 
-export default function page() {
+export default async function page() {
+    const session = await getServerSession(AuthConfig)
+
   return (
     <div>
-      Profile
+       <h1>Profile of {session?.user?.name}</h1>
+       {session?.user?.image &&  <img src={session?.user?.image!} alt="Image" />}
     </div>
   )
 }
